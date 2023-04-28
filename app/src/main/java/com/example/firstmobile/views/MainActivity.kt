84cd5cb1150dac5d8605b60806159a94acc731c4
3eight70 <@gbhfns90@gmail.com>
2023-04-28 12:48:54 +0700

    double click

--- app/src/main/java/com/example/firstmobile/views/MainActivity.kt
+++ app/src/main/java/com/example/firstmobile/views/MainActivity.kt
@@ -3,19 +3,22 @@ package com.example.firstmobile.views
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.firstmobile.ui.theme.BottomSheetShape
 import kotlinx.coroutines.launch
 
 
@@ -24,109 +27,125 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CodeBottomSheet()
-            RunBottomSheet()
+            MainLayout()
         }
     }
 }
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun CodeBottomSheet() {
-    val sheetState = rememberBottomSheetState(
-        initialValue = BottomSheetValue.Collapsed
-    )
-    val scaffoldState = rememberBottomSheetScaffoldState(
-        bottomSheetState = sheetState
-    )
+private fun MainLayout() {
     val scope = rememberCoroutineScope()
-    BottomSheetScaffold(
-        scaffoldState = scaffoldState,
-        sheetContent = {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(300.dp),
-                contentAlignment = Alignment.TopStart
-            ) {
-                Text(text = "Алексей", fontSize = 60.sp)
-            }
-        },
+    val scaffoldState = rememberBottomSheetScaffoldState()
+    var currentBottomSheet: BottomSheetScreen? by remember {
+        mutableStateOf(null)
+    }
 
-        sheetBackgroundColor = Color.Green,
-        sheetPeekHeight = 0.dp
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            contentAlignment = Alignment.BottomStart
-        ) {
-            FloatingActionButton(onClick = {
-                scope.launch {
-                    if (sheetState.isCollapsed) {
-                        sheetState.expand()
-                    } else {
-                        sheetState.collapse()
-                    }
-                }
-            }) {
-                Text(
-                    text = "+",
-                    fontSize = 30.sp
-                )  // Процент видимости: ${sheetState.progress.fraction}
+    if (scaffoldState.bottomSheetState.isCollapsed)
+        currentBottomSheet = null
+
+
+    val closeSheet: () -> Unit = {
+        scope.launch {
+            scaffoldState.bottomSheetState.collapse()
+
+        }
+    }
+
+
+    val openSheet: (BottomSheetScreen) -> Unit = {
+        scope.launch {
+            currentBottomSheet = it
+            scaffoldState.bottomSheetState.expand()
+        }
+
+    }
+
+    BottomSheetScaffold(sheetPeekHeight = 0.dp, scaffoldState = scaffoldState,
+        sheetShape = BottomSheetShape,
+        sheetContent = {
+            currentBottomSheet?.let { currentSheet ->
+                SheetLayout(currentSheet, closeSheet)
             }
+        }) { paddingValues ->
+        Box(Modifier.padding(paddingValues)) {
+            MainContent(openSheet)
         }
     }
 }
 
-@OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun RunBottomSheet() {
-    val sheetState = rememberBottomSheetState(
-        initialValue = BottomSheetValue.Collapsed
-    )
-    val scaffoldState = rememberBottomSheetScaffoldState(
-        bottomSheetState = sheetState
-    )
-    val scope = rememberCoroutineScope()
-    BottomSheetScaffold(
-        scaffoldState = scaffoldState,
-        sheetContent = {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(500.dp),
-                contentAlignment = Alignment.TopCenter
-            ) {
-                Text(text = "Здесь мог быть ваш результат", fontSize = 60.sp, color = Color.White)
-            }
-        },
-
-        sheetBackgroundColor = Color.Black,
-        sheetPeekHeight = 0.dp
+fun MainContent(openSheet: (BottomSheetScreen) -> Unit) {
+    Row(
+        Modifier.fillMaxSize().padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.Bottom
     ) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            contentAlignment = Alignment.BottomEnd
-        ) {
-            FloatingActionButton(onClick = {
-                scope.launch {
-                    if (sheetState.isCollapsed) {
-                        sheetState.expand()
-                    } else {
-                        sheetState.collapse()
-                    }
 
-                }
-            }) {
-                Text(
-                    text = ">",
-                    fontSize = 30.sp
-                )  // Процент видимости: ${sheetState.progress.fraction}
-            }
+        FloatingActionButton(onClick = {
+            openSheet(BottomSheetScreen.Screen1)
+        }) {
+            Text(
+                text = "+",
+                fontSize = 30.sp
+            )  // Процент видимости: ${sheetState.progress.fraction}
+        }
+
+        FloatingActionButton(onClick = {
+            openSheet(BottomSheetScreen.Screen2())
+        }) {
+            Text(
+                text = ">",
+                fontSize = 30.sp
+            )  // Процент видимости: ${sheetState.progress.fraction}
         }
     }
+}
+
+@Composable
+fun SheetLayout(currentScreen: BottomSheetScreen, onCloseBottomSheet: () -> Unit) {
+    when (currentScreen) {
+        BottomSheetScreen.Screen1 -> Screen1()
+        is BottomSheetScreen.Screen2 -> Screen2()
+    }
+}
+
+@Composable
+fun Screen1() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(300.dp)
+            .background(Color.Green, shape = RectangleShape)
+    ) {
+        Text(
+            text = "Блоки",
+            Modifier
+                .align(Alignment.Center)
+                .padding(16.dp), color = Color.Black,
+            fontSize = 15.sp
+        )
+    }
+}
+
+@Composable
+fun Screen2() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(500.dp)
+            .background(Color.Black, shape = RectangleShape)
+    ) {
+        Text(
+            text = "Здесь мог быть ваш результат",
+            Modifier
+                .align(Alignment.Center)
+                .padding(16.dp), color = Color.White, fontSize = 15.sp
+        )
+    }
+}
+
+sealed class BottomSheetScreen() {
+    object Screen1 : BottomSheetScreen()
+    class Screen2 : BottomSheetScreen()
 }
