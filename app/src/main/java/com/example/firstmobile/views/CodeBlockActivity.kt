0d9c87dbc90e2e8f27b5446db83e97bdeeb6ae15
Vasilev Danil <111416100+3eight70@users.noreply.github.com>
2023-04-28 16:50:39 +0700

    Bottom sheet menus
    Two menus for add block and console

--- app/src/main/java/com/example/firstmobile/views/CodeBlockActivity.kt
+++ app/src/main/java/com/example/firstmobile/views/CodeBlockActivity.kt
@@ -3,13 +3,79 @@ package com.example.firstmobile.views
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement.Bottom
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.BottomSheetScaffold
+import androidx.compose.material.BottomSheetValue
+import androidx.compose.material.Button
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Text
+import androidx.compose.material.rememberBottomSheetScaffoldState
+import androidx.compose.material.rememberBottomSheetState
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.firstmobile.R
+import kotlinx.coroutines.launch
 
+
+@ExperimentalMaterialApi
 class CodeBlockActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
+
         setContent {
-        
+            val sheetState = rememberBottomSheetState(
+                initialValue = BottomSheetValue.Collapsed
+
+            )
+            val scaffoldState = rememberBottomSheetScaffoldState(
+                bottomSheetState = sheetState
+            )
+            val scope = rememberCoroutineScope()
+            BottomSheetScaffold(
+                scaffoldState = scaffoldState,
+                sheetContent = {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(300.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(text = "Алексей хороший человек", fontSize = 60.sp)
+                    }
+                },
+                sheetBackgroundColor = Color(R.color.purple_200),
+                sheetPeekHeight = 0.dp
+            ) {
+                Box(
+                    modifier = Modifier.fillMaxSize().padding(16.dp),
+                    contentAlignment = Alignment.BottomStart
+                ) {
+                    FloatingActionButton(onClick = {
+                        scope.launch {
+                            if (sheetState.isCollapsed){
+                                sheetState.expand()
+                            } else{
+                                sheetState.collapse()
+                            }
+
+                        }
+                    }) {
+                        Text(text = "+")  // Процент видимости: ${sheetState.progress.fraction}
+                    }
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/firstmobile/views/MainActivity.kt
+++ app/src/main/java/com/example/firstmobile/views/MainActivity.kt
@@ -3,29 +3,145 @@ package com.example.firstmobile.views
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.*
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.firstmobile.ui.theme.BottomSheetShape
+import kotlinx.coroutines.launch
 
+
+@ExperimentalMaterialApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-        
+            MainLayout()
         }
     }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+private fun MainLayout() {
+    val scope = rememberCoroutineScope()
+    val scaffoldState = rememberBottomSheetScaffoldState()
+
+    var currentBottomSheet: BottomSheetScreen? by remember {
+        mutableStateOf(BottomSheetScreen.Screen1)
+    }
+
+    val closeSheet: () -> Unit = {
+        scope.launch {
+            scaffoldState.bottomSheetState.collapse()
+        }
+    }
+
+    val openSheet: (BottomSheetScreen) -> Unit = {
+        scope.launch {
+            currentBottomSheet = it
+            scaffoldState.bottomSheetState.expand()
+        }
+    }
+
+    BottomSheetScaffold(sheetPeekHeight = 0.dp, scaffoldState = scaffoldState,
+        sheetShape = BottomSheetShape,
+        sheetContent = {
+            currentBottomSheet?.let { currentSheet ->
+                SheetLayout(currentSheet, closeSheet)
+            }
+        }) { paddingValues ->
+        Box(Modifier.padding(paddingValues)) {
+            MainContent(openSheet)
+        }
+    }
+}
+
+@Composable
+fun MainContent(openSheet: (BottomSheetScreen) -> Unit) {
+    Row(
+        Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.Bottom
+    ) {
+
+        FloatingActionButton(onClick = {
+            openSheet(BottomSheetScreen.Screen1)
+        }) {
+            Text(
+                text = "+",
+                fontSize = 30.sp
+            )  // Процент видимости: ${sheetState.progress.fraction}
+        }
+
+        FloatingActionButton(onClick = {
+            openSheet(BottomSheetScreen.Screen2)
+        }) {
+            Text(
+                text = ">",
+                fontSize = 30.sp
+            )  // Процент видимости: ${sheetState.progress.fraction}
+        }
+    }
+}
+
+@Composable
+fun SheetLayout(currentScreen: BottomSheetScreen, onCloseBottomSheet: () -> Unit) {
+    when (currentScreen) {
+        BottomSheetScreen.Screen1 -> Screen1()
+        is BottomSheetScreen.Screen2 -> Screen2()
+    }
+}
+
+@Composable
+fun Screen1() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(300.dp)
+            .background(Color.Green, shape = RectangleShape)
+    ) {
+        Text(
+            text = "Блоки",
+            Modifier
+                .align(Alignment.Center)
+                .padding(16.dp), color = Color.Black,
+            fontSize = 15.sp
+        )
+    }
+}
+
+@Composable
+fun Screen2() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(500.dp)
+            .background(Color.Black, shape = RectangleShape)
+    ) {
+        Text(
+            text = "Здесь мог быть ваш результат",
+            Modifier
+                .align(Alignment.Center)
+                .padding(16.dp), color = Color.White, fontSize = 15.sp
+        )
+    }
+}
+
+sealed class BottomSheetScreen() {
+    object Screen1 : BottomSheetScreen()
+    object Screen2 : BottomSheetScreen()
 }
